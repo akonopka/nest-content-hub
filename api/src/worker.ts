1ae2f1863e2770cecafea38010449e486c555a96
Alicja Konopka <alicja.konopka@wp.pl>
2026-09-14 19:08:38 +0200

    Add worker microservice connected to RabbitMQ

---
+++ api/src/worker.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { WorkerModule } from './worker.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+async function bootstrap() {
+  let worker = await NestFactory.createMicroservice<MicroserviceOptions>(
+    WorkerModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL!],
+        queue: 'posts_embedding_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await worker.listen();
+}
+bootstrap();

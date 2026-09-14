a60fe7a42dc40b86a4cfb7672902d2970fbbe864
Alicja Konopka <alicja.konopka@wp.pl>
2026-09-14 17:33:51 +0200

    Add global PrismaModule providing PrismaService

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PrismaModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })

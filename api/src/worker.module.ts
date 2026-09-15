5b4b8b1a85352410e6d4f28af4893c47dd68cd73
Alicja Konopka <alicja.konopka@wp.pl>
2026-09-15 12:33:40 +0200

    Import PrismaModule and RabbitMQModule into WorkerModule so PostsService can be resolved

--- api/src/worker.module.ts
+++ api/src/worker.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsWorkerController } from './worker/posts-worker.controller';
 import { PostsModule } from './posts/posts.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, PrismaModule, RabbitMQModule],
   controllers: [PostsWorkerController],
   providers: [],
 })

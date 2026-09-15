7ebfbb13a57be07626974a9168852b425baa7c00
Alicja Konopka <alicja.konopka@wp.pl>
2026-09-15 10:20:37 +0200

    Ensure Qdrant collection exists on worker startup

--- api/src/worker.ts
+++ api/src/worker.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { WorkerModule } from './worker.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { QdrantClient } from '@qdrant/js-client-rest';
 
 async function bootstrap() {
   const worker = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -9,13 +10,26 @@ async function bootstrap() {
       transport: Transport.RMQ,
       options: {
         urls: [process.env.RABBITMQ_URL!],
-        queue: 'posts_embedding_queue',
+        queue: process.env.EMBEDDING_QUEUE!,
         queueOptions: {
           durable: false,
         },
       },
     },
   );
+
+  const client = new QdrantClient({ url: process.env.QDRANT_URL! });
+  try {
+    await client.getCollection(process.env.POSTS_COLLECTION!);
+  } catch (exception) {
+    await client.createCollection(process.env.POSTS_COLLECTION!, {
+      vectors: {
+        size: Number(process.env.POSTS_COLLECTION_SIZE!),
+        distance: 'Cosine',
+      },
+    });
+  }
+
   await worker.listen();
 }
 bootstrap();

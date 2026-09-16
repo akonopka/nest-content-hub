61d18d0ab53c981d13ae99ec8c81872c9be78a19
Alicja Konopka <alicja.konopka@wp.pl>
2026-09-16 18:03:43 +0200

    Export the OpenAPI spec to api/openapi.json on boot

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  writeFileSync('openapi.json', JSON.stringify(document, null, 2));
 
   await app.listen(process.env.PORT ?? 3000);
 }

274392f5d436f35e5f0dd5f9fb895f22253e3fac
Alicja Konopka <alicja.konopka@wp.pl>
2026-09-23 16:43:43 +0200

    Require email in AskDto for async response delivery

--- api/src/ask/ask.dto.ts
+++ api/src/ask/ask.dto.ts
@@ -1,7 +1,12 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AskDto {
   @IsString()
   @IsNotEmpty()
   question: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
